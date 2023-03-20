16a91e9b94d4169557a401e769106a50dfd2a188
Jose_Ramon_Gonzalez <pepeglezdevega@gmail.com>
2023-03-20 16:40:17 +0100

    a

--- client/src/helpers/axios.ts
+++ client/src/helpers/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import config from '@/config/config';
 import { authStore } from '@/stores/auth/authStore';
-
+/* Importamos variable de entorno */
 const BASE_URL = config.URL_BACKEND;
 const api = axios.create({
     baseURL: BASE_URL + "/api/boards",
